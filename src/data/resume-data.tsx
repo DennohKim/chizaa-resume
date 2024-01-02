cb6c944191ccd3fead76f62e3abaec4a6707071c
Dennis Kimathi <Kimathidennis38@gmail.com>
2024-01-02 23:08:24 +0300

    update to Chizaa

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,107 +1,104 @@
-import ConsultlyLogo from "../images/logos/consultly.svg";
-import AmbitLogo from "../images/logos/ambit.png";
-import BarepapersLogo from "../images/logos/barepapers.svg";
-import BimLogo from "../images/logos/bim.png";
-import CDGOLogo from "../images/logos/cdgo.png";
-import ClevertechLogo from "../images/logos/clevertech.png";
-import EvercastLogo from "../images/logos/evercast.svg";
-import Howdy from "../images/logos/howdy.png";
-import JarockiMeLogo from "../images/logos/jarocki.svg";
-import JojoMobileLogo from "../images/logos/jojomobile.png";
-import MonitoLogo from "../images/logos/monito.svg";
-import MobileVikingsLogo from "../images/logos/mv.png";
-import NSNLogo from "../images/logos/nsn.svg";
-import ParabolLogo from "../images/logos/parabol.svg";
-import TastyCloudLogo from "../images/logos/tastycloud.png";
-import YearProgressLogo from "../images/logos/yearprogress.svg";
-import Minimal from "../images/logos/minimal.svg";
+import ConsultlyLogo from "/public/images/logos/consultly.svg";
+import AmbitLogo from "/public/images/logos/ambit.png";
+import BarepapersLogo from "/public/images/logos/barepapers.svg";
+import BimLogo from "/public/images/logos/bim.png";
+import CDGOLogo from "/public/images/logos/cdgo.png";
+import ClevertechLogo from "/public/images/logos/clevertech.png";
+import EvercastLogo from "/public/images/logos/evercast.svg";
+import Howdy from "/public/images/logos/howdy.png";
+import JarockiMeLogo from "/public/images/logos/jarocki.svg";
+import JojoMobileLogo from "/public/images/logos/jojomobile.png";
+import MonitoLogo from "/public/images/logos/monito.svg";
+import MobileVikingsLogo from "/public/images/logos/mv.png";
+import NSNLogo from "/public/images/logos/nsn.svg";
+import ParabolLogo from "/public/images/logos/parabol.svg";
+import TastyCloudLogo from "/public/images/logos/tastycloud.png";
+import YearProgressLogo from "/public/images/logos/yearprogress.svg";
+import Minimal from "/public/images/logos/minimal.svg";
 import { GitHubIcon } from "@/components/icons/GitHubIcon";
 import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { XIcon } from "@/components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Dennis Kimathi",
+  initials: "DK",
+  location: "Nairobi, Kenya, KEN",
+  locationLink: "https://www.google.com/maps/place/Nairobi",
   about:
-    "Full Stack Engineer focused on building products with extra attention to details",
+    "Full Stack and blockchain Engineer with a passion for building products that not only address user problems but also align seamlessly with business needs.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "I am a highly-motivated, creative and pro-active designer and web developer able to research, design and develop user experiences for various digital products including mobile applications, websites and web applications. I use my broad range of skills to execute designs that are clean, simple and effective.",
+  avatarUrl:
+    "https://cdn.sanity.io/images/1p71u5pj/production/c99fba0287111e176f3fa1c653d3f8b446978aef-1080x1080.jpg?w=2000&fit=max&auto=format",
+  personalWebsiteUrl: "https://denniskimathi.dev",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "kimathidennis38@gmail.com",
+    tel: "+254 743312265",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/DennohKim",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/dennis-kimathi-46326711b/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/mbiebuz",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Moringa School",
+      degree: "Software Engineering",
+      start: "April 2022",
+      end: "Nov 2022",
+    },
+    {
+      school: "Jomo Kenyatta University of Agriculture and Technology",
+      degree: "Bachelor's Degree in Real Estate",
+      start: "2015",
+      end: "2019",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Silvi",
+      link: "https://www.silvi.earth/",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      title: "Senior Frontend Developer",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "August 2023",
+      end: "Present",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Focusing on developing user interfaces and enhancing user experiences. Collaborating with cross-functional teams to deliver efficient and responsive web applications. Utilizing technologies such as NextJS(React), TypeScript, and ensuring optimal integration with backend systems. ",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
+      company: "Chizaalabs",
+      link: "https://portfolio-amber-gamma.vercel.app/",
       badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      title: "UI/UX Consultant → Full Stack Developer",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "Apr 2017",
+      end: "July 2022",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Designed and built web apps for clients across the globe. Built a (decentralized finance)defi social trading app for Loibon from scratch. Technologies: NextJS, React, TypeScript, GraphQL, Solidity, Firebase, The Graph, ",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "Ryden International",
+      link: "https://ryden.co.ke/",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
+      title: "Digital Marketer",
       logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      start: "Jan 2020",
+      end: "Apr 2022",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Successfully executed and optimized digital marketing campaigns. Crafted and implemented strategies that led to a 30% increase in brand visibility and a 25% growth in user engagement. Utilized a mix of SEO, SEM, and social media marketing to achieve targeted goals. Implemented data-driven approaches, resulting in a 20% improvement in campaign performance.",
     },
   ],
   skills: [
@@ -110,169 +107,142 @@ export const RESUME_DATA = {
     "React/Next.js/Remix",
     "Node.js",
     "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Go",
+    "Solidity",
+    "Firebase/Supabase/AppWrite",
+    "Tailwind CSS",
+    "React Testing Library",
+    "Cypress",
+    "Git",
+    "Docker",
+    "Linux",
+    "AWS",
+    "GCP",
+    "Heroku",
+    "Vercel",
+    "DigitalOcean",
+    "MongoDB",
+    "PostgreSQL",
+    "Redis",
+    "The Graph",
+    "IPFS",
+    "OpenZeppelin",
+    "Uniswap",
+    "Chainlink",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Nafter",
       techStack: [
         "Side Project",
         "TypeScript",
         "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "React",
+        "Solidity",
+        "Celo",
       ],
-      description: "A platform to build and grow your online business",
+      description:
+        "A marketplace platform for users to buy and sell Non-Fungible-Tokens",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "Nafter",
+        href: "https://nft-marketplace-dof7f3erp-dennohkim.vercel.app/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "TumainiDAO",
+      techStack: ["Hackathon", "JavaScript", "React", "Solidity", "EthersJS"],
       description:
-        "Browser extension that records everything happening in a web application",
+        "TumainiDAO is a non-profit Decentralised Application Organization that looks to empower the African Girl Child ",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "TumainiDAO",
+        href: "https://tumainid-a-o.vercel.app/",
       },
     },
     {
-      title: "Jarocki.me",
+      title: "denniskimathi.dev",
       techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
+      description: "My personal website and blog. Built with Next.js and MDX",
       logo: JarockiMeLogo,
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "denniskimathi.dev",
+        href: "https://www.denniskimathi.dev/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
+      title: "juakalike",
+      techStack: ["Side Project", "Next.js", "Solidity", "celo", "wagmi hooks"],
+      description: "a marketplace dapp built on celo blockchain",
       logo: Minimal,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "juakalike",
+        href: "https://juakali-marketplace-d-app-react-app.vercel.app/",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "CarbonHero",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Hackathon",
+        "Next.js",
+        "ToucanSDK",
+        "Celo",
+        "Graphql",
+        "Graph Protocol",
+      ],
+      description: "a marketplace dapp built on celo blockchain",
+      logo: Minimal,
+      link: {
+        label: "carbonhero",
+        href: "https://toucan-protocol-sdk-react-app.vercel.app/",
+      },
+    },
+    {
+      title: "CeloDev Stream",
+      techStack: [
+        "Hackathon",
+        "Next.js",
+        "SuperfluidSDK",
+        "Celo",
+        "Hardhat",
+        "Solidity",
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "Token streaming dapp built with superfluid that rewards developers in celo ecosystem",
+      logo: Minimal,
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "CeloDevStream",
+        href: "https://codejam-streaming-rewards-react-app.vercel.app/",
       },
     },
     {
-      title: "Evercast",
+      title: "LumenPay",
+      techStack: ["React", "TailwindCSS", "NodeJS", "Express"],
+      description:
+        "A Crypto Payroll Platform to help employers to manage their payroll and pay their employees with crypto",
+      logo: Minimal,
+      link: {
+        label: "LumenPay",
+        href: "https://lumenpay-website.vercel.app/",
+      },
+    },
+    {
+      title: "Web3Guardian",
       techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Typescript",
+        "Web3",
+        "Telegram Bot",
+        "NodeJS",
+        "Ethers.js",
+        "UTU Protocol",
       ],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+        "A telegram bot that leverages the UTU Web3 Protocol to provide reputation checks on telegram users",
+      logo: Minimal,
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "Web3Guardian",
+        href: "https://t.me/web3guardian_utu_bot",
       },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
